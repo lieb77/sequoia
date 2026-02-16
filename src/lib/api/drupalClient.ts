92313e844989b974eb7c73b614d96871622f5fdb
Paul Lieberman <lieb77@gmail.com>
2026-02-16 01:46:47 +0000

    Needed to add deserialize to client

--- src/lib/api/drupalClient.ts
+++ src/lib/api/drupalClient.ts
@@ -5,21 +5,24 @@ import { ApiClient } from "./apiClient"
 import { base } from "../constants"
 
 export class DrupalClient extends ApiClient {
-    private client: NextDrupal;
+    private client: NextDrupal
 
     constructor(baseUrl: string) {
-        super();
-        this.client = new NextDrupal(baseUrl);
+        super()
+        this.client = new NextDrupal(baseUrl)
     }
 
     async getResourceCollection<T>(resourceType: string, options?: any): Promise<T[]> {
-        return this.client.getResourceCollection<T>(resourceType, options);
+        return this.client.getResourceCollection<T>(resourceType, options)
     }
     
     async getResource<T>(resourceType: string, options?: any): Promise<T[]> {
-        return this.client.getResource<T>(resourceType, options);
+        return this.client.getResource<T>(resourceType, options)
+    }
+    deserialize(body: any, options?: any) {
+        return this.client.deserialize(body)
     }
     // ... other methods using this.client
 }
 
-export const client = new DrupalClient(base);
+export const client = new DrupalClient(base)
