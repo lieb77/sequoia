ef82db522798916316773af9012d9c01eda85a8b
Paul Lieberman <lieb@paullieberman.org>
2026-02-19 11:54:21 -0800

    Added rel='me' and rel='webmention' links to the head metatdat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,46 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from '@/components/Navbar';
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+    variable: '--font-geist-sans',
+    subsets: ['latin']
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+    variable: '--font-geist-mono',
+    subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "Lieb's Log",
-  description: "An experiment in decoupled Drupal",
-};
+    title: "Lieb's Log",
+    description: 'An experiment in decoupled Drupal',
+    icons: {
+        other: [
+            {
+                rel: 'me',
+                url: 'https://paullieberman.net/about'
+            },
+            {
+                rel: 'webmention',
+                url: 'https://webmention.io/paullieberman.net/webmention'
+            }
+        ]
+    }
+}
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-    	  <Navbar />
-	  	<main className="p-4 mt-10 place-items-center">{children}</main>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <Navbar />
+                <main className="p-4 mt-10 place-items-center">{children}</main>
+            </body>
+        </html>
+    )
 }
